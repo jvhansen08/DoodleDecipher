986e3833c1b28ce20cafa37dab207f0d780782be
theaaronhanks <theaaronhanks@gmail.com>
2022-12-08 20:57:24 -0700

    fix error

--- app/src/main/java/com/jaredaaronlogan/myapplication/ui/repositories/LobbyRepo.kt
+++ app/src/main/java/com/jaredaaronlogan/myapplication/ui/repositories/LobbyRepo.kt
@@ -33,7 +33,7 @@ object LobbyRepo {
         val lobby = Lobby(
             hostId = UserRepository.getCurrentUserId(),
             joinCode = joinCode,
-            isGameStarted = false,
+            gameStarted = false,
         )
 
         lobbyRef.child(lobby.joinCode ?: "").setValue(lobby)
