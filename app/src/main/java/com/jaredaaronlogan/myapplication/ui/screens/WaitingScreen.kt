1d073dc136bc771a2179a630b8387c87684153aa
muffinman38 <jvhansen08@gmail.com>
2022-12-09 23:36:35 -0700

    end to home

--- app/src/main/java/com/jaredaaronlogan/myapplication/ui/screens/WaitingScreen.kt
+++ app/src/main/java/com/jaredaaronlogan/myapplication/ui/screens/WaitingScreen.kt
@@ -1,10 +1,7 @@
 package com.jaredaaronlogan.myapplication.ui.screens
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -24,9 +21,27 @@ fun WaitingScreen(navController: NavController, gameId: String) {
     println("Welcome to the waiting screen")
     viewModel.initialize(gameId)
     if (state.gameOver) {
-        Text(text = "Game Over")
-        Button(onClick = { navController.navigate(Routes.End.route + "?gameId$gameId") }) {
-            Text(text = "end")
+        Column(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth()
+                    .fillMaxHeight(.5f),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(text = "Game Over")
+
+            }
+            Row(
+                modifier = Modifier.fillMaxSize(),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Button(onClick = { navController.navigate(Routes.Home.route + "?gameId$gameId") }) {
+                    Text(text = "Return to main screen")
+                }
+            }
         }
     } else {
         if (!state.waiting) {
